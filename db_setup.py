8517cb4e757f8168cac1165a2cd5aea4d003338e
KWojcik243 <owcaaa243@gmail.com>
2023-04-06 22:50:38 +0200

    ADD: docstrings

--- db_setup.py
+++ db_setup.py
@@ -3,6 +3,15 @@ from decouple import config
 
 class DbSetup:
     def connect(self):
+        """
+        Connects to a PostgreSQL database using the provided credentials from the environment variables.
+
+        Returns:
+            conn (psycopg2.extensions.connection): A connection object to interact with the database.
+            
+        Raises:
+            psycopg2.Error: If the database connection fails.
+        """
         try:
             conn = psycopg2.connect(dbname=config("POSTGRES_DB_NAME"),
                                     user=config("POSTGRES_USER"),
@@ -15,6 +24,13 @@ class DbSetup:
         
 
     def create_table(self):
+        """
+        Creates a PostgreSQL table named "messages" with columns for id (SERIAL PRIMARY KEY), user_id (INTEGER), role (VARCHAR(20) NOT NULL), and message (TEXT NOT NULL).
+        
+        Raises:
+            psycopg2.Error: If the database connection fails or the table creation fails.
+        """
+        
         command = (
         """
         CREATE TABLE messages (
@@ -42,6 +58,17 @@ class DbSetup:
             print(error)
             
     def insert_mess(self,role ,message, user_id):
+        """
+        Inserts a new row into the "messages" table in the connected PostgreSQL database, containing the provided user_id, role, and message.
+
+        Args:
+            role (str): The role of the user who sent the message.
+            message (str): The text of the message sent by the user.
+            user_id (int): The unique identifier of the user who sent the message.
+        
+        Raises:
+            psycopg2.Error: If the database connection fails or the query execution fails.
+        """
         query= """ INSERT INTO messages (user_id, role, message) VALUES (%s, %s, %s)"""
         record = (user_id, role, message)
         try:
@@ -59,6 +86,15 @@ class DbSetup:
             print(error)
             
     def delete_mess(self,user_id):
+        """
+        Deletes one or more rows from the "messages" table in the connected PostgreSQL database that match the provided user_id.
+
+        Args:
+            user_id (int): The unique identifier of the user whose message(s) should be deleted.
+        
+        Raises:
+            psycopg2.Error: If the database connection fails or the query execution fails.
+        """
         query= """ DELETE FROM messages WHERE user_id = %s """
         record = [user_id]
         try:
@@ -77,6 +113,18 @@ class DbSetup:
             print(error)
             
     def get_user_mess(self, user_id):
+        """
+        Retrieves all rows from the "messages" table in the connected PostgreSQL database that match the provided user_id, and returns the role and message columns for each row.
+
+        Args:
+            user_id (int): The unique identifier of the user whose messages should be retrieved.
+        
+        Returns:
+            records (list of tuples): A list of tuples, where each tuple represents a matching row and contains the values of the role and message columns.
+        
+        Raises:
+            psycopg2.Error: If the database connection fails or the query execution fails.
+        """
         query= """ SELECT role, message FROM messages WHERE user_id = %s """
         record = [user_id]
         try:
@@ -97,9 +145,8 @@ class DbSetup:
             
 
 if __name__ == '__main__':
-    # try:
-    print("set")
-        # db_setup = DbSetup()
-    #     db_setup.create_table()
-    # except:
-    #     pass
+    try:
+        db_setup = DbSetup()
+        db_setup.create_table()
+    except:
+        pass
